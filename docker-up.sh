47db17874ecda46a2499631cac8b3ddd9180819d
root <root@DESKTOP-TCGHL6H.localdomain>
2021-07-15 17:10:50 -0600

    updates

--- docker-up.sh
+++ docker-up.sh
@@ -2,4 +2,4 @@
 
 # https://gist.github.com/raychorn/1075e0a4e73c7fb3aaa9400e23805522
 
-docker run -d -p 3040:9000 --name portainer --restart=always -v portainer_data:/data portainer/portainer-ce -H tcp://host.docker.internal:2375
+docker run -d -p 3040:9000 --cpus="1" --memory=512m --name portainer --restart=always -v portainer_data:/data portainer/portainer-ce -H tcp://host.docker.internal:2375
