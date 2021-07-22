e738f70eeceb366800cb4b94823f3dae72cef7c7
Ray C Horn <raychorn@gmail.com>
2021-07-22 04:19:54 -0600

    updates.

--- docker/portainer_ce/docker-up.sh
+++ docker/portainer_ce/docker-up.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-#docker-compose -H tcp://host.docker.internal:2375 up -d --force-recreate
-docker stack deploy -c portainer-agent-stack.yml portainer
+EXTERNAL_HOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
+
+#docker-compose -H tcp://$EXTERNAL_HOST:2375 up -d
+
+docker-compose -H tcp://host.docker.internal:2375 up -d
+#docker stack deploy -c portainer-agent-stack.yml portainer
+
+#docker-compose up -d
